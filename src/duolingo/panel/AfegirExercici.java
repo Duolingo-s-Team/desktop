d95c90707e88aa09a38e666cdad338705cee5712
AmadorMartinez10 <ttore10.amc@gmail.com>
2020-12-08 18:34:15 +0100

    New necessary files and classes.

---
+++ src/duolingo/panel/AfegirExercici.java
@@ -0,0 +1,165 @@
+package duolingo.panel;
+
+import java.awt.Dimension;
+import java.awt.GridLayout;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+import javax.swing.GroupLayout;
+import javax.swing.GroupLayout.Alignment;
+
+import org.json.simple.JSONObject;
+
+import javax.swing.JButton;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
+import javax.swing.JTree;
+
+import com.jgoodies.forms.layout.ColumnSpec;
+import com.jgoodies.forms.layout.FormLayout;
+import com.jgoodies.forms.layout.FormSpecs;
+import com.jgoodies.forms.layout.RowSpec;
+
+import exercisesForms.TestTypeForm;
+import implementations.ExerciseImpl;
+import implementations.LevelImpl;
+import interfaces.IExercise;
+import interfaces.ILevel;
+import json.JsonDecode;
+import models.Exercise;
+import models.Level;
+
+import java.awt.Font;
+
+public class AfegirExercici extends JPanel {
+
+	ILevel levelManager = new LevelImpl();
+	IExercise exerciseManager = new ExerciseImpl();
+	
+	public AfegirExercici(String[] labels) {
+
+		//new Dimension((int) (this.getToolkit().getScreenSize().getWidth() / 1.5), (int) (this.getToolkit().getScreenSize().getHeight() / 1.5));
+		setPreferredSize(new Dimension((int) (this.getToolkit().getScreenSize().getWidth() / 1.75), (int) (this.getToolkit().getScreenSize().getHeight() / 1.75) + 100));
+
+		JPanel Caracteristicas = new JPanel();
+		
+		JPanel Botones = new JPanel();
+		GroupLayout groupLayout = new GroupLayout(this);
+		groupLayout.setHorizontalGroup(
+			groupLayout.createParallelGroup(Alignment.LEADING)
+				.addGroup(groupLayout.createSequentialGroup()
+					.addContainerGap()
+					.addComponent(Caracteristicas, GroupLayout.PREFERRED_SIZE, 256, GroupLayout.PREFERRED_SIZE)
+					.addGap(18)
+					.addComponent(Botones, GroupLayout.DEFAULT_SIZE, 583, Short.MAX_VALUE)
+					.addContainerGap())
+		);
+		groupLayout.setVerticalGroup(
+			groupLayout.createParallelGroup(Alignment.LEADING)
+				.addGroup(groupLayout.createSequentialGroup()
+					.addContainerGap()
+					.addGroup(groupLayout.createParallelGroup(Alignment.LEADING)
+						.addComponent(Caracteristicas, Alignment.TRAILING, GroupLayout.DEFAULT_SIZE, 573, Short.MAX_VALUE)
+						.addComponent(Botones, Alignment.TRAILING, GroupLayout.DEFAULT_SIZE, 573, Short.MAX_VALUE))
+					.addContainerGap())
+		);
+
+		Caracteristicas.setLayout(new FormLayout(new ColumnSpec[] {
+				FormSpecs.RELATED_GAP_COLSPEC,
+				FormSpecs.DEFAULT_COLSPEC,
+				FormSpecs.RELATED_GAP_COLSPEC,
+				ColumnSpec.decode("max(127dlu;default):grow"),
+				FormSpecs.RELATED_GAP_COLSPEC,
+				FormSpecs.DEFAULT_COLSPEC,
+				FormSpecs.RELATED_GAP_COLSPEC,
+				FormSpecs.DEFAULT_COLSPEC,
+				FormSpecs.RELATED_GAP_COLSPEC,
+				FormSpecs.DEFAULT_COLSPEC,},
+			new RowSpec[] {
+				FormSpecs.RELATED_GAP_ROWSPEC,
+				FormSpecs.DEFAULT_ROWSPEC,
+				FormSpecs.RELATED_GAP_ROWSPEC,
+				FormSpecs.DEFAULT_ROWSPEC,
+				FormSpecs.RELATED_GAP_ROWSPEC,
+				FormSpecs.DEFAULT_ROWSPEC,
+				FormSpecs.RELATED_GAP_ROWSPEC,
+				FormSpecs.DEFAULT_ROWSPEC,
+				FormSpecs.RELATED_GAP_ROWSPEC,
+				FormSpecs.DEFAULT_ROWSPEC,
+				FormSpecs.RELATED_GAP_ROWSPEC,
+				FormSpecs.DEFAULT_ROWSPEC,
+				FormSpecs.RELATED_GAP_ROWSPEC,
+				FormSpecs.DEFAULT_ROWSPEC,
+				FormSpecs.RELATED_GAP_ROWSPEC,
+				FormSpecs.DEFAULT_ROWSPEC,
+				FormSpecs.RELATED_GAP_ROWSPEC,
+				FormSpecs.DEFAULT_ROWSPEC,
+				FormSpecs.RELATED_GAP_ROWSPEC,
+				FormSpecs.DEFAULT_ROWSPEC,
+				FormSpecs.RELATED_GAP_ROWSPEC,
+				RowSpec.decode("max(111dlu;default):grow"),}));
+		
+		JLabel lblNewLabel = new JLabel("Curso:");
+		lblNewLabel.setFont(new Font("Tahoma", Font.PLAIN, 18));
+		Caracteristicas.add(lblNewLabel, "2, 4");
+		
+		JLabel cursolbl = new JLabel(labels[0]);
+		cursolbl.setFont(new Font("Tahoma", Font.PLAIN, 18));
+		Caracteristicas.add(cursolbl, "4, 6");
+		
+		JLabel lblNewLabel_1 = new JLabel("Categor\u00EDas:");
+		lblNewLabel_1.setFont(new Font("Tahoma", Font.PLAIN, 18));
+		Caracteristicas.add(lblNewLabel_1, "2, 8");
+		
+		JLabel categoriaslbl = new JLabel(labels[1]);
+		categoriaslbl.setFont(new Font("Tahoma", Font.PLAIN, 18));
+		Caracteristicas.add(categoriaslbl, "4, 10");
+		
+		JLabel lblNewLabel_2 = new JLabel("Nivel");
+		lblNewLabel_2.setFont(new Font("Tahoma", Font.PLAIN, 18));
+		Caracteristicas.add(lblNewLabel_2, "2, 12");
+		
+		JLabel nivellbl = new JLabel(labels[2]);
+		nivellbl.setFont(new Font("Tahoma", Font.PLAIN, 18));
+		Caracteristicas.add(nivellbl, "4, 14");
+		
+		JLabel lblNewLabel_3 = new JLabel("Arbol de categor\u00EDas y niveles del curso");
+		Caracteristicas.add(lblNewLabel_3, "2, 18, 3, 1");
+		
+		JTree tree = new JTree();
+		Caracteristicas.add(tree, "2, 22, 3, 1, fill, fill");
+		Botones.setLayout(new GridLayout(0, 2, 10, 10));
+		
+		JButton btnNewButton_1 = new JButton("TIPUS TEST");
+		Botones.add(btnNewButton_1);
+		
+		btnNewButton_1.addActionListener(new ActionListener() {
+			
+			@Override
+			public void actionPerformed(ActionEvent arg0) {
+				TestTypeForm ttf = new TestTypeForm(labels[2]);
+				ttf.setVisible(true);
+			}
+		});
+		
+		JButton btnNewButton_2 = new JButton("New button");
+		Botones.add(btnNewButton_2);
+		
+		JButton btnNewButton_4 = new JButton("New button");
+		Botones.add(btnNewButton_4);
+		
+		JButton btnNewButton = new JButton("New button");
+		Botones.add(btnNewButton);
+		
+		JButton btnNewButton_5 = new JButton("New button");
+		Botones.add(btnNewButton_5);
+		
+		JButton btnNewButton_6 = new JButton("New button");
+		Botones.add(btnNewButton_6);
+		
+		JButton btnNewButton_3 = new JButton("New button");
+		Botones.add(btnNewButton_3);
+		setLayout(groupLayout);
+		
+	}
+}
---
+++ src/exercisesForms/TestTypeForm.java
@@ -0,0 +1,161 @@
+package exercisesForms;
+
+import java.awt.BorderLayout;
+import java.awt.Dimension;
+import java.awt.EventQueue;
+import java.awt.Toolkit;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+import javax.swing.border.EmptyBorder;
+
+import org.hibernate.engine.spi.ExecutableList;
+import org.json.simple.JSONObject;
+
+import implementations.ExerciseImpl;
+import implementations.LevelImpl;
+import implementations.UserImpl;
+import interfaces.IExercise;
+import interfaces.ILevel;
+import interfaces.IUser;
+import json.JsonDecode;
+import json.JsonEncode;
+import models.Exercise;
+import models.Level;
+
+import com.jgoodies.forms.layout.FormLayout;
+import com.jgoodies.forms.layout.ColumnSpec;
+import com.jgoodies.forms.layout.RowSpec;
+import com.jgoodies.forms.layout.FormSpecs;
+import javax.swing.JLabel;
+import javax.swing.JTextField;
+import javax.swing.JButton;
+
+public class TestTypeForm extends JFrame {
+
+	private JPanel contentPane;
+	private JTextField questionField;
+	private JLabel correctLabel;
+	private JTextField correctField;
+	private JLabel incorrect1Label;
+	private JTextField incorrect1Field;
+	private JLabel incorrect2Label;
+	private JTextField incorrect2Field;
+	private JButton saveExerciseButton;
+
+	public TestTypeForm(String levelName) {
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		contentPane = new JPanel();
+		contentPane.setMinimumSize(new Dimension((int) (this.getToolkit().getScreenSize().getWidth() / 1.5), (int) (this.getToolkit().getScreenSize().getHeight() / 1.5)));
+		contentPane.setPreferredSize(new Dimension((int) (this.getToolkit().getScreenSize().getWidth() / 1.5), (int) (this.getToolkit().getScreenSize().getHeight() / 1.5)));
+		contentPane.setBorder(new EmptyBorder(25, 25, 25, 25));
+		setContentPane(contentPane);
+		contentPane.setLayout(new FormLayout(new ColumnSpec[] {
+				FormSpecs.RELATED_GAP_COLSPEC,
+				FormSpecs.DEFAULT_COLSPEC,
+				FormSpecs.RELATED_GAP_COLSPEC,
+				FormSpecs.DEFAULT_COLSPEC,
+				FormSpecs.RELATED_GAP_COLSPEC,
+				FormSpecs.DEFAULT_COLSPEC,
+				FormSpecs.RELATED_GAP_COLSPEC,
+				FormSpecs.DEFAULT_COLSPEC,
+				FormSpecs.RELATED_GAP_COLSPEC,
+				FormSpecs.DEFAULT_COLSPEC,
+				FormSpecs.RELATED_GAP_COLSPEC,
+				FormSpecs.DEFAULT_COLSPEC,
+				FormSpecs.RELATED_GAP_COLSPEC,
+				FormSpecs.DEFAULT_COLSPEC,
+				FormSpecs.RELATED_GAP_COLSPEC,
+				FormSpecs.DEFAULT_COLSPEC,
+				FormSpecs.RELATED_GAP_COLSPEC,
+				ColumnSpec.decode("default:grow"),
+				FormSpecs.RELATED_GAP_COLSPEC,
+				ColumnSpec.decode("default:grow"),},
+			new RowSpec[] {
+				FormSpecs.RELATED_GAP_ROWSPEC,
+				FormSpecs.DEFAULT_ROWSPEC,
+				FormSpecs.RELATED_GAP_ROWSPEC,
+				FormSpecs.DEFAULT_ROWSPEC,
+				FormSpecs.RELATED_GAP_ROWSPEC,
+				FormSpecs.DEFAULT_ROWSPEC,
+				FormSpecs.RELATED_GAP_ROWSPEC,
+				FormSpecs.DEFAULT_ROWSPEC,
+				FormSpecs.RELATED_GAP_ROWSPEC,
+				FormSpecs.DEFAULT_ROWSPEC,
+				FormSpecs.RELATED_GAP_ROWSPEC,
+				FormSpecs.DEFAULT_ROWSPEC,
+				FormSpecs.RELATED_GAP_ROWSPEC,
+				FormSpecs.DEFAULT_ROWSPEC,
+				FormSpecs.RELATED_GAP_ROWSPEC,
+				FormSpecs.DEFAULT_ROWSPEC,
+				FormSpecs.RELATED_GAP_ROWSPEC,
+				FormSpecs.DEFAULT_ROWSPEC,
+				FormSpecs.RELATED_GAP_ROWSPEC,
+				FormSpecs.DEFAULT_ROWSPEC,
+				FormSpecs.RELATED_GAP_ROWSPEC,
+				FormSpecs.DEFAULT_ROWSPEC,
+				FormSpecs.RELATED_GAP_ROWSPEC,
+				FormSpecs.DEFAULT_ROWSPEC,}));
+		
+		JLabel questionLabel = new JLabel("Frase a traduir:");
+		contentPane.add(questionLabel, "14, 8");
+		
+		questionField = new JTextField();
+		contentPane.add(questionField, "18, 8, fill, default");
+		questionField.setColumns(11);
+		
+		correctLabel = new JLabel("Frase traduïda correcta:");
+		contentPane.add(correctLabel, "14, 12");
+		
+		correctField = new JTextField();
+		contentPane.add(correctField, "18, 12, fill, default");
+		correctField.setColumns(10);
+		
+		incorrect1Label = new JLabel("Frase incorrecta 1:");
+		contentPane.add(incorrect1Label, "14, 16");
+		
+		incorrect1Field = new JTextField();
+		contentPane.add(incorrect1Field, "18, 16, fill, default");
+		incorrect1Field.setColumns(10);
+		
+		incorrect2Label = new JLabel("Frase incorrecta 2:");
+		contentPane.add(incorrect2Label, "14, 20");
+		
+		incorrect2Field = new JTextField();
+		contentPane.add(incorrect2Field, "18, 20, fill, default");
+		incorrect2Field.setColumns(10);
+		
+		saveExerciseButton = new JButton("Guardar exercici");
+		contentPane.add(saveExerciseButton, "18, 24");
+		
+		saveExerciseButton.addActionListener(new ActionListener() {
+			
+			Level level;
+			
+			@Override
+			public void actionPerformed(ActionEvent arg0) {
+				
+				ILevel levelManager = new LevelImpl();
+				IExercise exerciseManager = new ExerciseImpl();
+				
+				level = levelManager.getLevelByName(levelName);
+				
+				if (!questionField.equals("") || !correctField.equals("") || !incorrect1Field.equals("") || !incorrect2Field.equals("")) {
+					String content = JsonEncode.jsonContentInsertTest("TIPUS_TEST", questionField.getText(), 
+							correctField.getText(), incorrect1Field.getText()+","+incorrect2Field.getText());
+					
+					exerciseManager.insertExercise(new Exercise("TEST_EX", 20, content, level));
+					
+					setVisible(false);
+				}
+				
+			}
+		});
+		
+		
+		
+	}
+
+}
